71c4dc2430da970ac5d7cce3043148bccc792eb5
GirlyTechGeek <53258022+GirlyTechGeek@users.noreply.github.com>
2020-08-28 17:01:05 +0000

    Azubi React Quiz

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+// state data for 3 counters
+const data = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 },
+];
+
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+  render() {
+    return (
+      <div>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+        <button onClick={this.IncrementItem}>-</button>
+        <button onClick={this.DecreaseItem}>+</button>
+        
+       
+        
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+ 
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+
+        {data.map((counter) => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+      </div>
+    );
+  }
+
+}
+
+
+export default App;
